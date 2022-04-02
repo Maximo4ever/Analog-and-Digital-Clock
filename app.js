9707f604afba4303c52d23dc57f266bfbe380c95
Maximo-4ever <maximilianocm06@gmail.com>
2022-04-02 16:06:26 -0600

    Agrega la funcionalidad al reloj digital

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ const $hoursEl = document.getElementById("hours");
 const $minutesEl = document.getElementById("minutes");
 const $secondsEl = document.getElementById("seconds");
 
+const $hoursDigitalEl = document.getElementById("digitalHour");
+const $minutesDigitalEl = document.getElementById("digitalMinutes");
+const $secondsDigitalEl = document.getElementById("digitalSeconds");
+const $AmPm = document.getElementById("AmPm")
+
 setInterval(() => {
   const day = new Date();
-  const hours = day.getHours() * 30;
-  const minutes = day.getMinutes();
-  const seconds = day.getSeconds();
+  let hours = day.getHours();
+  let minutes = day.getMinutes();
+  let seconds = day.getSeconds();
+  let am = hours >= 12 ? "PM" : "AM"
   
+  // Digital clock
+  if (hours > 12) hours = hours - 12
+  $hoursDigitalEl.innerHTML = hours < 10 ? "0"+hours : hours
+  $minutesDigitalEl.innerHTML = minutes < 10 ? "0"+minutes : minutes
+  $secondsDigitalEl.innerHTML = seconds < 10 ? "0"+seconds : seconds
+  $AmPm.innerHTML = am
+
+  // Analog clock
+  hours = hours * 30;
+  minutes = minutes * 6;
+  seconds = seconds * 6;
   $hoursEl.style.transform = `rotateZ(${hours+(minutes/12)}deg)`;
   $minutesEl.style.transform = `rotateZ(${minutes}deg)`
   $secondsEl.style.transform = `rotateZ(${seconds}deg)`
+
 }, 1000);
